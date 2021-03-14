de6d2984583dad82ed70450485e907cacda92411
arlanoliveir <79853949+arlanoliveir@users.noreply.github.com>
2021-03-13 22:23:57 -0300

    Update install.sh

--- install.sh
+++ install.sh
@@ -1,12 +1,10 @@
 #!/usr/bin/bash
 pkg upgrade
-cd
 git clone https://github.com/arlanoliveir/base2.git
 clear
-cd teste2
+cd base2
 bash install.sh
-cd
-cd teste2
+cd base2
 npm start
 
 echo "[*] obrigado por utilizar o bot \"arlanbot\" arlan agradece"
